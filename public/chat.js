03a647b0f6bb0d850b5f9225b2c9f3019330063d
Kevin Wong <kkwokwai22@gmail.com>
2017-07-17 16:33:10 -0700

    finish product of websocket with boardcast message implementation

--- public/chat.js
+++ public/chat.js
@@ -7,12 +7,11 @@ var message = document.getElementById('message')
     handle = document.getElementById('handle')
     btn = document.getElementById('send')
     output = document.getElementById('output')
+    feedback = document.getElementById('feedback')
 
 // Emit events
 
 btn.addEventListener('click', function() {
-  console.log(message);
-  console.log(handle);
   socket.emit('chat', {
     message: message.value,
     handle: handle.value
@@ -20,7 +19,18 @@ btn.addEventListener('click', function() {
 });
 
 
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+});
+
+
 // Listen for events
 socket.on('chat', function(data){
+  feedback.innerHTML = '';
   output.innerHTML += '<p><strong>' + data.handle + '</strong> ' + data.message + '</p>';
-})
+});
+
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' + data + ' is typing .....</em></p>';
+});
